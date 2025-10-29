9bf9a2e386bd395469103dfbda5381e0f4d588d5
Sumit tripathi <sumittripathi8795@gmail.com>
2025-10-29 18:47:29 +0530

    i learn a very easy concept when use send request to server then create a log file

--- day2/index.js
+++ day2/index.js
@@ -1,9 +1,25 @@
 const http = require("http")
+const fs = require("fs");
+const { error } = require("console");
 
 const PORT = 7050
 
 const server = http.createServer((request, response) => {
-    response.end("Helloo from server")
+
+    // response.end("Helloo from server")
+
+    // This is an assignment -> when use send request to server then create a log file 
+    const log = `${Date.now()} : & From ${request.url} New Request Recieved\n`;
+
+    fs.appendFile("logs.txt", log, (err) => {
+        if (err) {
+            console.error("Error writing to the log file: ", err)
+            response.statusCode = 500; // server error 
+            response.end("Internal server Error")
+            return;
+        }
+        response.end("Hello world from server")
+    })
 })
 
 server.listen(PORT, () => {
